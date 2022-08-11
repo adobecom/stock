b45bbc76764cf98377a95be317087d25bf6479ea
Chris Millar <cmillar@adobe.com>
2022-08-11 14:45:10 -0600

    Small updates from college

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,22 +1,44 @@
-const branch = new URLSearchParams(window.location.search).get('milolibs') || 'main';
+/*
+ * Copyright 2022 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 
-export default function getMiloLibs() {
-  const { hostname } = window.location;
-  if (!hostname.includes('hlx.page')
-    && !hostname.includes('hlx.live')
-    && !hostname.includes('localhost')) return '/libs';
-  return branch === 'local' ? 'http://localhost:6456/libs' : `https://${branch}.milo.pink/libs`;
-}
+// This can be changed to 'https://milo.adobe.com/libs'
+// if you don't have your /libs mapped to the milo origin.
+const PROD_LIBS = 'https://milo.adobe.com/libs';
 
 const config = {
+  // imsClientId: 'college',
   projectRoot: `${window.location.origin}/`,
   locales: {
     '': { ietf: 'en-US', tk: 'hah7vzn.css' },
     de: { ietf: 'de-DE', tk: 'hah7vzn.css' },
     cn: { ietf: 'zh-CN', tk: 'puu3xkp' },
   },
-  miloLibs: getMiloLibs(),
 };
+
+/*
+ * ------------------------------------------------------------
+ * Edit below at your own risk
+ * ------------------------------------------------------------
+ */
+
+function getMiloLibs() {
+  const { hostname } = window.location;
+  if (!hostname.includes('hlx.page')
+    && !hostname.includes('hlx.live')
+    && !hostname.includes('localhost')) return PROD_LIBS;
+  const branch = new URLSearchParams(window.location.search).get('milolibs') || 'main';
+  return branch === 'local' ? 'http://localhost:6456/libs' : `https://${branch}.milo.pink/libs`;
+}
+config.miloLibs = getMiloLibs();
 
 (async function loadStyle() {
   const link = document.createElement('link');
