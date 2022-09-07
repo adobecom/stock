41ddd12de6d29dc1e3b3b55242190ca01e69495e
Solène Duprat <solene.duprat@gmail.com>
2022-09-07 09:33:17 -0400

    re-organize my pull request

--- pages/blocks/page-feed-card/page-feed-card.js
+++
@@ -1,5 +0,0 @@
-export default async function decorate($block) {
-  const $section = $block.closest('main > .section');
-  $block.remove();
-  if ($section.childElementCount === 0) $section.remove();
-}
