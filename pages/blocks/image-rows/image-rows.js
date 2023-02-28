970b32c4ccac6f2722a1546b46afa5b75d2e4d1f
Solène Duprat <solene.duprat@gmail.com>
2023-02-28 16:18:42 -0500

    testing analytics on the image-rows block

--- pages/blocks/image-rows/image-rows.js
+++ pages/blocks/image-rows/image-rows.js
@@ -1,4 +1,8 @@
+import { getLibs } from '../../scripts/utils.js';
+const { decorateBlockAnalytics } = await import(`${getLibs()}/martech/attributes.js`);
+
 export default function imageRows(block) {
+  decorateBlockAnalytics(block);
   Array.from(block.children).forEach((div) => {
     if (!div.querySelector('img')) div.classList.add('image-rows-caption');
   });
